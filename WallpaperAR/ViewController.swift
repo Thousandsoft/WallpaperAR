8b966808769bc03edcdce7f08ab7ad5d13b0f3ab
Thousandsoft <77388518+Thousandsoft@users.noreply.github.com>
2021-01-26 21:03:23 +0500

    Wall search added

--- WallpaperAR/ViewController.swift
+++ WallpaperAR/ViewController.swift
@@ -9,66 +9,76 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController{
 
     @IBOutlet var sceneView: ARSCNView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var planes = [Plane]()
         
-        // Set the view's delegate
-        sceneView.delegate = self
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Set the view's delegate
+            sceneView.delegate = self
+            
+            // Show statistics such as fps and timing information
+            sceneView.showsStatistics = true
+            
+            sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+            
+            sceneView.autoenablesDefaultLighting = true
+            
+            // Create a new scene
+            let scene = SCNScene()
+            
+            // Set the scene to the view
+            sceneView.scene = scene
+        }
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            // Create a session configuration
+            let configuration = ARWorldTrackingConfiguration()
+            
+            configuration.planeDetection = .vertical
 
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+            // Run the view's session
+            sceneView.session.run(configuration)
+        }
         
-        // Pause the view's session
-        sceneView.session.pause()
-    }
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            
+            // Pause the view's session
+            sceneView.session.pause()
+        }
+}
+
 
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+
+extension ViewController: ARSCNViewDelegate {
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+            
+            guard anchor is ARPlaneAnchor else { return }
+            
+            let plane = Plane(anchor: anchor as! ARPlaneAnchor)
+            
+            self.planes.append(plane)
+            node.addChildNode(plane)
+        }
         
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+        func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+            
+            let plane = self.planes.filter { plane in
+                return plane.anchor.identifier == anchor.identifier
+            }.first
+            
+            guard plane != nil else { return }
+            
+            plane?.update(anchor: anchor as! ARPlaneAnchor)
+        }
 }
+
